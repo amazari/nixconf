0daad3959c318c62665517429d83f38bbec91a8d
scaroo <scaroo@gmail.com>
2019-03-14 22:27:18 +0100

    typo

--- roles/kodi.nix
+++ roles/kodi.nix
@@ -9,7 +9,7 @@
   hardware.pulseaudio.enable = true;
   services.xserver.enable = true;
   services.xserver.desktopManager.kodi.enable = true;
-  nixpkgs.config.kodi.enableControllers = true;
+#  nixpkgs.config.kodi.enableControllers = true;
   nixpkgs.config.kodi.enableYatp = true;
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.displayManager.lightdm.greeter.enable = false;
